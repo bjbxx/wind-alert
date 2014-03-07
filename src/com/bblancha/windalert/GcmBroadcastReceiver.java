08f8fcc49a9e55357269a3343b5ee7eea16a1c9a
Brian Blanchard <brian.blanch@gmail.com>
2014-03-06 19:47:06 -0500

    First Commit

---
+++ src/com/bblancha/windalert/GcmBroadcastReceiver.java
@@ -0,0 +1,46 @@
+/*
+ *  $URL: https://athena.redprairie.com/svn/prod/devtools/trunk/bootstrap/eclipse/codetemplates.xml $
+ *  $Author: mlange $
+ *  $Date: 2009-06-18 22:49:22 -0500 (Thu, 18 Jun 2009) $
+ *  
+ *  $Copyright-Start$
+ *
+ *  Copyright (c) 2014
+ *  RedPrairie Corporation
+ *  All Rights Reserved
+ *
+ *  This software is furnished under a corporate license for use on a
+ *  single computer system and can be copied (with inclusion of the
+ *  above copyright) only for use on such a system.
+ *
+ *  The information in this document is subject to change without notice
+ *  and should not be construed as a commitment by RedPrairie Corporation.
+ *
+ *  RedPrairie Corporation assumes no responsibility for the use of the
+ *  software described in this document on equipment which has not been
+ *  supplied or approved by RedPrairie Corporation.
+ *
+ *  $Copyright-End$
+ */
+
+package com.bblancha.windalert;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+
+}
